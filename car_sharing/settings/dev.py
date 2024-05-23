584555c30a828d5c53187c8f71d008495a10b9b7
Vitalii <vitalikbashkiser@gmail.com>
2024-05-24 02:39:29 +0300

    edit root static

--- car_sharing/settings/dev.py
+++ car_sharing/settings/dev.py
@@ -153,7 +153,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles"
 
 CSRF_TRUSTED_ORIGINS = [config('CSRF_TRUSTED_ORIGINS')]
 
